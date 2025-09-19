38d19f871041d2af511fc36ff570f50a08381ed3
asyjota221i1-a11y <ww@gmail.com>
2025-09-19 13:41:54 +0000

    studentmachine send

---
+++ cpp/zad1/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout<<"witaj swiecie"<<endl;
+	cout<<"podaj wiek"<<endl;
+	int x;
+	cin>>x;
+	cout<<"Podaj wiek nastepnej osoby"<<endl;
+	int y;
+	cin>>y;
+	cout<<"w sumie macie lat "<<x+y<<endl;
+	if(x+y >100){
+	cout<<"macie w sumie więcej niż 100"<<endl;
+	}
+	else{
+	count<<"macie mniej lub 100 lat"<<endl;
+	}
+		if(x>y){
+		count<<"osoba pierwsza jest starsza"<<endl;
+		}
+			if(x<y){
+		count<<"osoba druga jest starsza"<<endl;
+			}
+
+
+	return 0;
+}

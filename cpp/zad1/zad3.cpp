0e7b35b9de20aab25efc838fcd8aec126331dd4c
asyjota221i1-a11y <asyjota_221i1@e.zseeim.edu.pl>
2025-11-14 14:37:08 +0000

    studentmachine send

--- cpp/zad1/zad3.cpp
+++ cpp/zad1/zad3.cpp
@@ -5,26 +5,16 @@ using namespace std;
 
 int main(){
 	string imie;
-	string nazwisko;
 	cout<<"Podaj imie: "<<endl;
 	cin>>imie;
 	cout<<"Podaj nazwisko: "<<endl;
 	cin>>nazwisko;
-	ofstream plik("nazwy.txt", ios::trunc);
+	ofstream plik("nazwy.txt");
 	if (!plik.is_open()){
 	cout<<"Nie otwarto pliku\n"<<endl;
 	return 1;
 	}
 plik<<imie<<" "<<nazwisko;
-cout<<"Czy chcesz coś jeszcze dodać?"<<endl;
-string odpowiedz;
-cin>>odpowiedz;
-	if(odpowiedz=="tak"){
-	cout<<"dyktuj dalej..."<<endl;
-	cin>>odpowiedz;
-	plik<<" "<<odpowiedz;
-	}else{
-		plik.close();
-			}
+plik.close();
 	return 0;
 }
